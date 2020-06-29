5c891644ece07771709bc23fb3402befeb04c8ae
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-06-29 19:03:31 +0300

    Created the 'menu-item' component

---
+++ src/components/menu-item/menu-item.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="content">
+      <h2 className="title">{title.toUpperCase()}</h2>
+      <p className="subtitle">SHOP NOW</p>
+    </div>
+  </div>
+);
+
+MenuItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  size: PropTypes.string,
+};
+
+MenuItem.defaultProps = {
+  size: '',
+};
+
+export default MenuItem;
