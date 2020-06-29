614d45f4b3cac5bb66197eb30bb95a548a9c482d
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-06-29 19:35:57 +0300

    Added the 'menu-item.scss'
    Imported the styles into the 'menu-item' component

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './menu-item.scss';
+
 const MenuItem = ({ title, imageUrl, size }) => (
   <div className={`${size} menu-item`}>
     <div
