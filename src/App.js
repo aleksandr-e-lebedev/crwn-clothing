d2d94ca173c41382998a9cfbe795fe9b54f2d30c
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-06-30 18:32:47 +0300

    Created the 'homepage' page
    Imported the 'homepage' into the 'App.js'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import HomePage from './pages/homepage/homepage';
+
 import './App.css';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
