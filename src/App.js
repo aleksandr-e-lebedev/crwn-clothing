631aaa8d94c688af226519183373ff775ad2f4ef
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-07-05 18:32:42 +0300

    Created a temporary HatsPage for routing demonstration
    Updated the 'App.js'

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import HomePage from './pages/homepage/homepage';
 
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
       </Switch>
     </div>
   );
