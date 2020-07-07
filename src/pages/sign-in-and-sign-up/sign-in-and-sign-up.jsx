475112d782b0d3519274d5c33f768ab244908bdf
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-07-07 16:05:53 +0300

    Added the 'sign-in-and-sign-up.scss'
    Imported the styles into the 'sign-in-and-sign-up' page

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in';
 
+import './sign-in-and-sign-up.scss';
+
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
