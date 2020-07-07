6bb5e61eb841e339ed51b21b29553ad080caab8b
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-07-07 15:55:18 +0300

    Created the 'form-input' component

---
+++ src/components/form-input/form-input.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FormInput = ({ type, name, value, required, handleChange, label }) => (
+  <div className="group">
+    <input
+      className="form-input"
+      onChange={handleChange}
+      {...{ type, name, value, required }}
+    />
+    {label ? (
+      <label className={`${value.length ? 'shrink' : ''} form-input-label`}>
+        {label}
+      </label>
+    ) : null}
+  </div>
+);
+
+FormInput.propTypes = {
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  required: PropTypes.bool,
+  handleChange: PropTypes.func.isRequired,
+  label: PropTypes.string,
+};
+
+FormInput.defaultProps = {
+  required: false,
+  label: '',
+};
+
+export default FormInput;
