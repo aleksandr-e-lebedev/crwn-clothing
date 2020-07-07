c5eb2009bfc187779067253c3bf4614ac2f60b2b
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-07-07 16:07:17 +0300

    Imported the 'sign-in-and-sign-up' page into the 'App.js'

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 
 import Header from './components/header/header';
 
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
