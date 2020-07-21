3e4074e51d1a60757232c520a8a37f588f55a18d
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-07-21 16:29:21 +0300

    Updated the 'custom-button' component
    Added the 'isGoogleSignIn' prop

--- src/components/custom-button/custom-button.jsx
+++ src/components/custom-button/custom-button.jsx
@@ -3,8 +3,12 @@ import PropTypes from 'prop-types';
 
 import './custom-button.scss';
 
-const CustomButton = ({ children, type }) => (
-  <button className="custom-button" type={type}>
+const CustomButton = ({ children, type, handleClick, isGoogleSignIn }) => (
+  <button
+    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+    type={type}
+    onClick={handleClick}
+  >
     {children}
   </button>
 );
@@ -12,6 +16,13 @@ const CustomButton = ({ children, type }) => (
 CustomButton.propTypes = {
   children: PropTypes.node.isRequired,
   type: PropTypes.oneOf(['button', 'submit', 'reset']).isRequired,
+  handleClick: PropTypes.func,
+  isGoogleSignIn: PropTypes.bool,
+};
+
+CustomButton.defaultProps = {
+  handleClick: () => {},
+  isGoogleSignIn: false,
 };
 
 export default CustomButton;
