c6561f09de76e11b8c046ae755e054e809cda381
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-08-09 23:47:30 +0300

    Updated the 'header' component
    Imported:
    1) 'cart-icon' component;
    2) 'cart-dropdown' component;
    3) 'current user' selector;
    4) 'cart hidden' selector.
    
    Connected to the Redux store

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import PropTypes from 'prop-types';
 
+import CartIcon from '../cart-icon/cart-icon';
+import CartDropdown from '../cart-dropdown/cart-dropdown';
+
 import { auth } from '../../firebase/firebase.utils';
+
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <header className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -29,7 +37,9 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </header>
 );
 
@@ -38,10 +48,16 @@ Header.propTypes = {
     displayName: PropTypes.string,
     email: PropTypes.string,
   }),
+  hidden: PropTypes.bool.isRequired,
 };
 
 Header.defaultProps = {
   currentUser: null,
 };
 
-export default Header;
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden,
+});
+
+export default connect(mapStateToProps)(Header);
