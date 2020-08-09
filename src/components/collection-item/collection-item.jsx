8c7d02d7eb0948f37ea471c35785c777e8b29943
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-08-09 23:38:43 +0300

    Updated the 'collection-item' component and its styles
    Imported:
    1) 'custom-button' component;
    2) 'add item' cart action.
    
    Connected to the Redux store

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,27 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+
+import CustomButton from '../custom-button/custom-button';
+
+import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.scss';
 
-const CollectionItem = ({ name, imageUrl, price }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
-    </div>
-  </div>
-);
+// eslint-disable-next-line no-shadow
+const CollectionItem = ({ item, addItem }) => {
+  const { name, imageUrl, price } = item;
 
-CollectionItem.propTypes = {
-  name: PropTypes.string.isRequired,
-  imageUrl: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">${price}</span>
+      </div>
+      <CustomButton type="button" handleClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
+    </div>
+  );
 };
 
-export default CollectionItem;
+CollectionItem.propTypes = {
+  item: PropTypes.shape({
+    name: PropTypes.string,
+    imageUrl: PropTypes.string,
+    price: PropTypes.number,
+  }).isRequired,
+  addItem: PropTypes.func.isRequired,
+};
+
+const mapDispatch = {
+  addItem,
+};
+
+export default connect(null, mapDispatch)(CollectionItem);
