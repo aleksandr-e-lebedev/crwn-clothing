7b2f19670d1a2183332ab79d8ee4d2dfc64d5c99
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-08-09 23:11:39 +0300

    Implemented a Redux store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import store from './redux/store';
 
 import App from './App';
 
 import './index.css';
 
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
