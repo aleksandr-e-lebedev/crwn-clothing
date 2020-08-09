520a6299826389198489468d21407c71b89dea61
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-08-09 22:52:12 +0300

    Implemented 'user' actions
    Includes:
    1) set current user.

---
+++ src/redux/user/user.actions.js
@@ -0,0 +1,7 @@
+import UserActionTypes from './user.types';
+
+// eslint-disable-next-line import/prefer-default-export
+export const setCurrentUser = (user) => ({
+  type: UserActionTypes.SET_CURRENT_USER,
+  payload: user,
+});
