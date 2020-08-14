e04382a0373667e6034bd0758b838107d99d8e2b
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-08-14 15:39:10 +0300

    Updated the 'shop' page
    Removed state
    Converted to functional component
    Moved the 'collection-preview' rendering to the 'collection-overview'
    
    Imported:
    1) 'collection-overview' component;
    2) 'collection-page' component.

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,29 +1,21 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import CollectionsOverview from '../../components/collections-overview/collections-overview';
+import CollectionPage from '../collection/collection';
 
-import SHOP_DATA from './shop.data';
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, title, routeName, items }) => (
-          <CollectionPreview key={id} {...{ title, routeName, items }} />
-        ))}
-      </div>
-    );
-  }
-}
+ShopPage.propTypes = {
+  match: PropTypes.shape({
+    path: PropTypes.string,
+  }).isRequired,
+};
 
 export default ShopPage;
