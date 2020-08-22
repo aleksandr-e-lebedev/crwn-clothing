623d1cd981482f79cb6f86f7d5796252469e5786
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-08-22 21:14:57 +0300

    Updated the 'shop' page component
    Converted the component to a class component.
    Connected the component to the Redux store.
    Wrapped the CollectionsOverview and CollectionPage components in the WithSpinner HOC.
    Implemented an Observer pattern using the Firestore library to get shop data from Firestore.

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,21 +1,82 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
+
+import WithSpinner from '../../components/with-spinner/with-spinner';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview';
 import CollectionPage from '../collection/collection';
 
-const ShopPage = ({ match }) => (
-  <div>
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+import {
+  firestore,
+  convertCollectionsShapshotToMap,
+} from '../../firebase/firebase.utils';
+
+import { updateCollections } from '../../redux/shop/shop.actions';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    // eslint-disable-next-line no-shadow
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection('collections');
+
+    this.unsubscribeFromSnapshot = collectionRef.onSnapshot((snapshot) => {
+      const collectionsMap = convertCollectionsShapshotToMap(snapshot);
+
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromSnapshot();
+  }
+
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
+
+    return (
+      <div>
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          render={(props) => (
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+      </div>
+    );
+  }
+}
 
 ShopPage.propTypes = {
+  updateCollections: PropTypes.func.isRequired,
   match: PropTypes.shape({
     path: PropTypes.string,
   }).isRequired,
 };
 
-export default ShopPage;
+const mapDispatch = { updateCollections };
+
+export default connect(null, mapDispatch)(ShopPage);
