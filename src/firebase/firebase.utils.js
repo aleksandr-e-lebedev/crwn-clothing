ca1ab611da7f7438c0138430abf1f5209f8ca990
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-09-14 18:05:28 +0300

    Updated the 'firebase.utils.js'
    Created a 'get current user' utility, which is a Promise-oriented solution that Saga can yield for
    Removed the 'sign in with Google' utility

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -18,17 +18,16 @@ firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
 
-export const createUserProfileDocument = async (userAuth, additionalData) => {
+export const getUserProfileReference = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
-  const snapShot = await userRef.get();
+  const userSnapshot = await userRef.get();
 
-  if (!snapShot.exists) {
+  const createUserProfileDocument = async () => {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
@@ -43,10 +42,21 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
       // eslint-disable-next-line no-console
       console.log('error creating user', error.message);
     }
-  }
+  };
+
+  if (!userSnapshot.exists) createUserProfileDocument();
 
   // eslint-disable-next-line consistent-return
   return userRef;
+};
+
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      unsubscribe();
+      resolve(userAuth);
+    }, reject);
+  });
 };
 
 export const addCollectionAndDocuments = async (
@@ -64,19 +74,19 @@ export const addCollectionAndDocuments = async (
   return batch.commit();
 };
 
-export const convertCollectionsShapshotToMap = (collections) => {
-  const transformedCollection = collections.docs.map((doc) => {
-    const { title, items } = doc.data();
+export const convertCollectionsShapshotToMap = (collectionsShapshot) => {
+  const transformedCollections = collectionsShapshot.docs.map((docShapshot) => {
+    const { title, items } = docShapshot.data();
 
     return {
       routeName: encodeURI(title.toLowerCase()),
-      id: doc.id,
+      id: docShapshot.id,
       title,
       items,
     };
   });
 
-  return transformedCollection.reduce((accumulator, collection) => {
+  return transformedCollections.reduce((accumulator, collection) => {
     accumulator[collection.title.toLowerCase()] = collection;
     return accumulator;
   }, {});
