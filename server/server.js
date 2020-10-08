8531dbd203f546dfb71ac3967a4bfa30b2506214
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-10-08 19:05:01 +0300

    Created 'server.js' in the server directory

---
+++ server/server.js
@@ -0,0 +1,29 @@
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+require('dotenv').config();
+
+const app = require('./app');
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED. Shutting down gracefully...');
+  server.close(() => {
+    console.log('Process terminated!');
+  });
+});
